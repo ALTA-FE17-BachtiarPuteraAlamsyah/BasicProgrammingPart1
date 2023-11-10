85dde7aa0d7eb8c62425208700b61f5c505eb47e
bachtiaralamsyah10@gmail.com <bachtiaralamsyah10@gmail.com>
2023-11-10 19:27:49 +0700

    TugasDay3

---
+++ Typescript/HelloWorld.ts
@@ -0,0 +1,26 @@
+// console.log("Hello World");
+
+// problem 3
+// let nama = "Kobar"
+
+// console.log("Hello" + " " + nama + "!");
+
+// problem 4
+// let alas = 20
+// let tinggi = 25
+
+// let luas_segitiga = 0.5 * alas * tinggi
+// console.log(luas_segitiga);
+
+//probelem 5
+// let T = 20
+// let r = 4
+// const phi = 3.14
+
+// // let LP_Tabung = (2 * phi * r * r) + (2 * phi * r * T)
+// let LP_Tabung = 2 * phi * r * (r + T)
+
+// console.log(LP_Tabung);
+
+
+
